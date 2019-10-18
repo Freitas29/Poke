1568eb56f0a707c3a3f35d0587f37143211c08b2
Freitas29 <arao.freitas@hotmail.com>
2019-10-18 15:03:19 -0300

    Added header component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
@@ -21,8 +20,17 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+body, html{
+  height: 100%;
+  background-color: #d1d8e0;
+}
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  color: #d1d8e0;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header id="app">
+        <a href="">Logo</a>
+        <ul>
+            <button>Entrar</button>
+        </ul>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+
+}
+</script>
+
+<style scoped>
+    header{
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        background-color: #a55eea;
+        height: 60px;
+    }
+</style>
