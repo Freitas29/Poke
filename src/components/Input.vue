07dcf49325a0c56bcdfe764ea5924eab6e50cc0c
Freitas29 <arao.freitas@hotmail.com>
2019-10-18 23:54:25 -0300

    Input component

---
+++ src/components/Input.vue
@@ -0,0 +1,41 @@
+<template>
+    <input
+        v-bind="$attrs"
+        v-bind:value="value"
+        v-on="inputListeners"
+      >  
+</template>
+
+<script>
+export default {
+    props: ['value'],
+    computed: {
+        inputListeners: function(){
+            var vm = this
+            return Object.assign({},
+                this.$listeners,
+                {
+                    input: function (event) {
+                        vm.$emit('input', event.target.value)
+                    }
+                }
+            )
+        }
+    },
+
+}
+</script>
+
+<style scoped>
+    input{
+        border: none;
+        padding: 12px 15px;
+        border-radius: 15px;
+        outline: none;
+        width: 50%;
+        background-color: #2c2932;
+        font-weight: bold;
+        color: #fff;
+        font-size: 14px;
+    }
+</style>
