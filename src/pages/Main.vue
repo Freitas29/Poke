a9d5214201d15d4f37453ae948e48ce52281860f
Freitas29 <arao.freitas@hotmail.com>
2019-10-18 22:52:36 -0300

    added methods to seach a pokemon

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,30 +1,62 @@
 <template>
   <div class="main">
+    <div class="actions">
+      <input v-model="name" />
+      <button v-on:click="search">Aa</button>
+    </div>
     <Card
-    image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png"
-    name="Charizard"
-    gender="Fogo"
+      v-if="pokemon"
+      v-bind:image="pokemon.sprites.front_default"
+      v-bind:name="pokemon.name"
+      v-bind:genders="pokemon.types"
      />
   </div>
 </template>
 
 <script>
 import Card from '../components/Card'
+import axios from 'axios'
 
 export default {
   name: 'Main',
   components: {
       Card
-  }
+  },
+  data(){
+        return { 
+            pokemon: null,
+            name: '',
+        }
+    },
+    computed: {
+      nameLowerCase: function(){
+        return this.name.toLowerCase()
+      }
+    },
+    methods: {
+      search: async function(){
+        try{
+          const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${this.nameLowerCase}/`)
+          this.pokemon = response.data
+        }catch(e){
+            alert(e)
+        }
+      }
+    }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .main{
-    display: grid;
-    grid-template-columns: repeat(3,1fr);
     padding: 5% 5%;
     grid-gap: 1%;
+}
+
+.main .actions{
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
